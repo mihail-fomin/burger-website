c41160ab862094ed635f62abc55c42f22b8df678
misha133kv <mihail_fomin_95@mail.ru>
2023-04-12 20:30:32 +0500

    added reducer logic

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,20 @@ import { Header } from "./components/Header/Header"
 import { Navigation } from "./components/Navigation/Navigation"
 import { Catalog } from "./components/Catalog/Catalog"
 import { Footer } from "./components/Footer/Footer"
+import { Provider } from "react-redux"
+import { store } from "./store/index.js"
 
 export const App = () => {
 
-	return <>
-		<Header />
-		<main>
-			<Navigation />
-			<Catalog />
-		</main>
-		<Footer />
-	</>
+	return (
+		<Provider store={store}>
+			<Header />
+			<main>
+				<Navigation />
+				<Catalog />
+			</main>
+			<Footer />
+		</Provider>
+	)
 }
 
