7c1a182b04e019a3669f6be1ccbe95d6be65e90e
misha133kv <mihail_fomin_95@mail.ru>
2023-04-13 22:53:31 +0500

    added productSlice

--- src/components/Catalog/CatalogProduct/CatalogProduct.jsx
+++ src/components/Catalog/CatalogProduct/CatalogProduct.jsx
@@ -1,17 +1,18 @@
+import { API_URI } from '../../../const'
 import style from './CatalogProduct.module.css'
 
-export const CatalogProduct = (props) => (
+export const CatalogProduct = ({ item }) => (
 
 	<article className={style.product}>
-		<img src="../../assets/img/photo-5.jpg" alt={props.title} className={style.image} />
+		<img src={`${API_URI}/${item.image}`} alt={item.title} className={style.image} />
 
-		<p className={style.price}>689<span className="currency">₽</span></p>
+		<p className={style.price}>{item.price}<span className="currency">₽</span></p>
 
 		<h3 className={style.title}>
-			<button className={style.detail}>{props.title}</button>
+			<button className={style.detail}>{item.title}</button>
 		</h3>
 
-		<p className={style.weight}>520г</p>
+		<p className={style.weight}>{item.weight}</p>
 
 		<button className={style.add} type="button">Добавить</button>
 	</article>
