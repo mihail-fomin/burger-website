331acbef0667ffe9d9e8dc53d1052afcd5dee12f
misha133kv <mihail_fomin_95@mail.ru>
2023-04-15 14:45:41 +0500

    added chart logic and edited styles

--- src/store/order/orderSlice.js
+++ src/store/order/orderSlice.js
@@ -1,8 +1,13 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import { API_URI, POSTFIX } from "../../const";
 
 
 const initialState = {
 	orderList: JSON.parse(localStorage.getItem('order') || '[]'),
+	orderGoods: [],
+	totalPrice: 0,
+	totalCount: 0,
+	error: [],
 };
 
 export const localStorageMiddleware = store => next => action => {
@@ -13,6 +18,17 @@ export const localStorageMiddleware = store => next => action => {
 	}
 	return nextAction
 }
+
+export const orderRequestAsync = createAsyncThunk(
+	'order/fetch',
+	(_, { getState }) => {
+		const listId = getState().order.orderList.map(item => item.id)
+		return fetch(`${API_URI}${POSTFIX}?list=${listId}`)
+			.then(req => req.json())
+			.catch(error => ({ error }))
+	}
+)
+
 
 const orderSlice = createSlice({
 	name: 'order',
@@ -26,6 +42,46 @@ const orderSlice = createSlice({
 				state.orderList.push({ ...action.payload, count: 1 })
 			}
 		}
+	},
+	extraReducers: builder => {
+		builder
+			.addCase(orderRequestAsync.pending, (state) => {
+				state.error = ''
+			})
+			.addCase(orderRequestAsync.fulfilled, (state, action) => {
+				const orderGoods = state.orderList.map(item => {
+					const product = action.payload
+						.find(product => product.id === item.id)
+
+					product.count = item.count
+
+					return product
+				})
+				state.error = ''
+				state.orderGoods = orderGoods
+
+				state.totalCount = orderGoods.reduce(
+					(acc, item) => acc + item.count
+					, 0
+				)
+				state.totalPrice = orderGoods.reduce(
+					(acc, item) => acc + item.count * item.price
+					, 0
+				)
+
+				// [state.totalCount, state.totalPrice] = orderGoods.reduce(
+				// 	([totalCount, totalPrice], item) => {
+				// 		const sumCount = totalCount + item.count
+				// 		const sumPrice = totalPrice + item.price * item.count
+
+				// 		return [sumCount, sumPrice]
+				// 	},
+				// 	[0, 0],
+				// );
+			})
+			.addCase(orderRequestAsync.rejected, (state, action) => {
+				state.error = action.payload.error
+			})
 	}
 })
 
