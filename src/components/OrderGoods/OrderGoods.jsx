aef5b47ca0eb774bcb1d93029ddea866c63f926b
misha133kv <mihail_fomin_95@mail.ru>
2023-04-16 13:07:51 +0500

    fixed styles

--- src/components/OrderGoods/OrderGoods.jsx
+++ src/components/OrderGoods/OrderGoods.jsx
@@ -6,13 +6,13 @@ export const OrderGoods = ({ title, price, image, count, id, weight }) => (
 	<li className={style.item}>
 		<img className={style.image} src={`${API_URI}/${image}`} alt={title} />
 
-		<div className="goods">
+		<div className={style.goods}>
 			<h3 className={style.title}>{title}</h3>
 
 			<p className={style.weight}>{weight}г</p>
 
 			<p className={style.price}>{price}
-				<span className="currency">&nbsp;₽</span>
+				<span>&nbsp;₽</span>
 			</p>
 		</div>
 
